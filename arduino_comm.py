3e981ae3f7c3905cff13429746080acf4030e316
WIN\madshv <mads-vandborg@hotmail.com>
2023-01-25 11:34:08 +0100

    Comments added to the code

--- arduino_comm.py
+++ arduino_comm.py
@@ -3,10 +3,14 @@
 Created on Tue Sep 13 12:56:57 2022
 
 @author: madshv
-"""
 
-import serial
-import io
+This program is used to write commands to the Arduino temperature controller for 2 piezo stages.
+The piezo stages requires a voltage from 0-5 V, and each piezo stage has 3 axes, meaning that
+3 separate voltages must be supplied to each stage. In total 6 voltages to the 2 stages must
+be controlled.
+
+This program is an example of how to write a command to the Arduino from the a computer
+using serial usb connection. The arduino has 6 PWM outputs with the following notation:
 
 # Arduino pwm pins
 # stage 1, X = pin D6
@@ -16,29 +20,46 @@ import io
 # stage 2, Y = pin D11
 # stage 2, Z = pin D3
 
+To make the program work.
+1) Change the COM port such that it matches with the used USB port.
+2) Set the duty cycle to an number in the range 0-1, linearly corresponding to a voltage of 0-5 V.
+3) Set the stage to ether 1 or 2
+4) Set the direction to either 'X', 'Y' or 'Z'
+
+"""
+
+
+import serial
+import io
+
+# Function used for serial reading.
 def read_buf(Ser):
     return Ser.read(Ser.inWaiting())
 
+# Function used for sending a duty cycle command to the Arduino
+# Ser: Serial object.
+# stage_no: 1 or 2
+# direction: 'X', 'Y' or 'Z'
+# duty cycle: Number between 0-1.
+
 def write_duty(Ser,stage_no,direction,duty_cycle):
-    # voltage: 0-5 V
-    # direction: 'X', 'Y' or 'Z'
-    # stage_no: '1' or '2'
-    
-    pwm_byte = duty_cycle*255
-    msg = direction+stage_no+str(int(pwm_byte))
+    pwm_byte = duty_cycle*255   # Calculate duty cycle as number between 0-255
+    msg = direction+stage_no+str(int(pwm_byte)) # Code the message to the Arduino
     msg = msg.encode('ascii')
-    Ser.write(msg)
+    Ser.write(msg)  # Serial write the message
 
 # Establish serial connection
-port = 'COM8'
+port = 'COM8'   # CHANGE TO APPROPRIATE PORT
 
+#### DO NOT CHANGE THE FOLLOWING #############
 ser = serial.Serial()
 ser.port = port
-ser.baudrate = 115200
+ser.baudrate = 115200   # Must match with the Arduino serial baudrate.
 ser.timeout = 1
 
 if ser.is_open == False:
     ser.open()
+##############################################
 
 # Example of how to write to Arduino
 duty_cycle = 0.5
--- pwm_control/pwm_control.ino
+++ pwm_control/pwm_control.ino
@@ -1,21 +1,45 @@
 
+/*
+January 2023
+@author: madshv
 
-int pwmPin = 5;
+NB: When loading a program to the arduino, remove the capacitor between reset and ground.
+When running the program, ensure to put a capacitor between reset and ground. Otherwise, problems
+will occur when opening and closing the serial connection while the Arduino program is running!
+
+This program is used to control the temperature of two piezo stages, each with three voltage inputs.
+The piezo stage temperatures are controlled by a voltage, which is supplied from PWM Arduino output.
+The Arduino has 6 PWM outputs, 3 for each piezo stage.
+The program takes USB serial commands from the arduino_comm.py script, and sets the PWM outputs
+hereafter.
+
+The stages must be connected to the following PWM pins:
+# stage 1, X = pin D6
+# stage 1, Y = pin D5
+# stage 1, Z = pin D9
+# stage 2, X = pin D10
+# stage 2, Y = pin D11
+# stage 2, Z = pin D3
+*/
+
+// Define global variables
 uint8_t input_byte=10;
 String str,str_dir,str_num;
 
-// When loading a program to the arduino, remove the capacitor between reset and ground.
-
+// The setup() function is run once when the arduino is powered.
+// In this function, the three timers of the atmega328p are set to 64 kHz PWM mode.
+// For each timer, two PWM outputs are available.
+// The meaning of the registers can be found in the datasheet of the atmega328p.
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);   // Must match with the python program serial baudrate.
   
   // Set timer0 registers
   // OC0A=D6, OC0B=D5
   pinMode(6,OUTPUT);
   pinMode(5,OUTPUT);
-  OCR0A = 50;
-  OCR0B = 150;
-  TCCR0A = (1<<COM0A1); //Set OC1A on compare match, clear OC1A at BOTTOM, (non-inverting mode).
+  OCR0A = 50;   // Set output register A to 50/255 PWM duty cycle
+  OCR0B = 150;  // Set output register B to 150/255 PWM duty cycle
+  TCCR0A = (1<<COM0A1);   //Set OC1A on compare match, clear OC1A at BOTTOM, (non-inverting mode).
   TCCR0A |= (1<<COM0B1); //Set OC1B on compare match, clear OC1B at BOTTOM, (non-inverting mode).
   TCCR0A |= (1<<WGM01)|(1<<WGM00); //Fast PWM
   TCCR0B = (1<<CS00);  // Prescaler=1
@@ -24,8 +48,8 @@ void setup() {
   // OC1A=D9, OC1B=D10
   pinMode(9,OUTPUT);
   pinMode(10,OUTPUT);
-  OCR1A = 50;
-  OCR1B = 150;
+  OCR1A = 50;   // Set output register A to 50/255 PWM duty cycle
+  OCR1B = 150;  // Set output register B to 150/255 PWM duty cycle
   TCCR1A = (1<<COM1A1); //Set OC1A on compare match, clear OC1A at BOTTOM, (non-inverting mode).
   TCCR1A |= (1<<COM1B1); //Set OC1B on compare match, clear OC1B at BOTTOM, (non-inverting mode).
   TCCR1A |= (1<<WGM10); //Fast PWM, 8-bit
@@ -35,8 +59,8 @@ void setup() {
   // OC2A=D11, OC2B=D3
   pinMode(11,OUTPUT);
   pinMode(3,OUTPUT);
-  OCR2A = 50;
-  OCR2B = 150;
+  OCR2A = 50;   // Set output register A to 50/255 PWM duty cycle
+  OCR2B = 150;  // Set output register B to 150/255 PWM duty cycle
   TCCR2A = (1<<COM2A1); //Set OC2A on compare match, clear OC2A at BOTTOM, (non-inverting mode).
   TCCR2A |= (1<<COM2B1); //Set OC2B on compare match, clear OC2B at BOTTOM, (non-inverting mode).
   TCCR2A |= (1<<WGM21)|(1<<WGM20); //Fast PWM
@@ -44,12 +68,20 @@ void setup() {
   
 }
 
+// The loop() function runs continiously in loop once after the setup() function has been run.
+// In this function the incoming serial messages are read, and based on the incoming string,
+// the PWM duty cycle of one of the PWM pins is set.
 void loop() {
-  while(Serial.available()>0){
+  while(Serial.available()>0){  // read while there are messages available in the serial buffer.
+    // Read and split the incoming string.
     str = Serial.readString();
     str_dir = str.substring(0,2);
     str_num = str.substring(2);
+    
+    // convert to an unsigned byte.
     input_byte = uint8_t(str_num.toInt());
+
+    // Set one of the PWM duty cycles.
     if(str_dir=="X1"){
       OCR0A = input_byte; //OC0A=D6
     }
@@ -68,6 +100,7 @@ void loop() {
     else if(str_dir=="Z2"){
       OCR2B = input_byte; //OC2B=D3
     }
+    // Send the read string components back (used for debugging)
     Serial.println(str_dir);
     Serial.println(str_num);
   }
